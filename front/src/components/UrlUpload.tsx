3d27df7f8710f6bea596a8082ef80de5bafd90f0
Croissantee <cdoron30@gmail.com>
2025-04-04 16:53:38 +0800

    Update

--- front/src/components/UrlUpload.tsx
+++ front/src/components/UrlUpload.tsx
@@ -496,6 +496,7 @@ const UrlUpload: React.FC<UrlUploadProps> = ({
           <svg className="w-4 h-4 mr-1 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
           </svg>
+          Supported URL Types
         </h3>
       </div>
     </div>
